b7be1be7d33db6328a27d9cb78f677f4bdcf1c41
Claudiu Cirmaci <claudiu.cirmaci@fortech.ro>
2022-06-03 13:21:36 +0300

    add navbar

--- iRecycleItUI/src/app/app.module.ts
+++ iRecycleItUI/src/app/app.module.ts
@@ -1,20 +1,26 @@
+import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LocationsComponent } from './locations/locations.component';
 import { AgmCoreModule } from '@agm/core';
+import { environment } from './../environments/environment';
+import { MatToolbarModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LocationsComponent
+    LocationsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AgmCoreModule.forRoot({
-      apiKey: "AIzaSyD1csME-8N42m83P-3GHRp6Oq2cKMyEKYk"
-    })
+      apiKey: environment.maps_api_key
+    }),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
